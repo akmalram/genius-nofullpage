44cae3a63cc7b15db8e2b285a8fff47a2d2d7f6b
akmal <islamov_akmal@inbox.ru>
2020-01-10 12:47:51 +0500

    asdf

--- js/script.js
+++ js/script.js
@@ -50,11 +50,6 @@ ready(() => {
         autoplay: {
             disableOnInteraction: false
         },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-            dynamicBullets: true
-        },
         breakpoints: {
             1020: {
                 slidesPerView: 3,
